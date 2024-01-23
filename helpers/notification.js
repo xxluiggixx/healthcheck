70b39e1e777c8978480acbb475e984d586d66efd
Luis María García Salado <lgarciasaladokuhl@outlook.com>
2024-01-23 14:30:19 -0300

    fix date

--- helpers/notification.js
+++ helpers/notification.js
@@ -5,7 +5,7 @@ const SLACK_ENABLE = Boolean(process.env.SLACK_ENABLE) || false;
 const notification = async (msg) =>{
     if(msg!=''){
         if (EMAIL_ENABLE) mail(msg);
-        if (SLACK_ENABLE) slackSendMessage(`${msg} has been restart at *${date()}*`);
+        if (SLACK_ENABLE) slackSendMessage(`${msg} has been restart at *${date}*`);
     }
 }
 

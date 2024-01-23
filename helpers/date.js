374a26527d5e75fdde25c6b6bab90dea3773abff
Luis María García Salado <lgarciasaladokuhl@outlook.com>
2024-01-23 15:13:32 -0300

    Fix

--- helpers/date.js
+++ helpers/date.js
@@ -1,6 +1,6 @@
 
 
-function date(){
+export function date(){
     const timestamp =new Date().getTime(); // Obtener el valor de tiempo en milisegundos
     const date = new Date(timestamp);
     const year = date.getFullYear(); // Obtener el año (por ejemplo, 2023)

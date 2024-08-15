76801ce357c1c6c014b12095d88d78c1f770feda
Luis María García Salado <lgarciasaladokuhl@outlook.com>
2024-08-15 11:51:34 -0300

    (Fix v1.5.0): Add reboot feature at vsphere.js

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
 require('dotenv').config()
 const {app} = require('./app');
 const TIME_INTERVAL = process.env.TIME_INTERVAL || 60000; // 1min
-const { Server, date, notification, Vsphere } = require('./helpers');
 
 
-//setInterval(app,TIME_INTERVAL);
- const mostrar = async () => {
-    const vmv = new Vsphere();
-    const  { vm } = await vmv.VmStatus("WIN10-PJS")
-    const resp = await vmv.VmReboot(vm)
-    console.log(resp)
- }
 
- mostrar()
+setInterval(app,TIME_INTERVAL);
